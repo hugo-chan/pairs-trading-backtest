c32df3f31f92fa27064646c67f9fae0c54a597ce
hugo-chan <hugoyhc14@gmail.com>
2020-07-09 11:08:19 +0800

    get history done

--- src/history.py
+++ src/history.py
@@ -1,7 +1,7 @@
 import requests
 import quandl
 import pandas as pd
-from datetime import date
+import datetime
 import os
 import configparser
 
@@ -22,14 +22,14 @@ class History:
         }
     }
 
-    def __init__(self, name):
+    def __init__(self, name):        
         self.name = name
         self.code = self.match.get(self.name).get("code") # get subscription code
         self.update()
 
 
     def get_history(self, start_date):
-        today = date.today()
+        today = datetime.date.today()
 
         price_label = self.match.get(self.name).get("price_label")
         df = quandl.get(self.code, start_date=start_date, end_date=str(today))[[price_label]] # get data
@@ -37,6 +37,8 @@ class History:
         # rename to same index name and variable name
         df.index.names = ['Date']
         df.rename(columns = {df.columns[0]: "Price?"}, inplace = True)
+        # remove nan rows
+        df.dropna(subset=["Price?"], inplace=True)
 
         return df
 
@@ -45,7 +47,7 @@ class History:
         class ColumnNameError(Exception):
             pass
 
-        folder_dir = "./data"
+        folder_dir = "../data"
         if not os.path.exists(folder_dir):
             os.mkdir(folder_dir)
 
@@ -53,15 +55,23 @@ class History:
 
         try:
             self.df = pd.read_csv(data_dir)
-            print(self.df)
+
+            print("orig", self.df)
+            # print(self.df)
             if list(self.df.columns) != ['Date', 'Price?']:
+                # csv file has not been initialized
                 raise ColumnNameError
             else:
-                last_date = self.df['Date'].iloc[self.df['Price?'].last_valid_index()] # last date with price defined
-                print(last_date)
                 # get last date with price defined
-                # update from last date to today
+                last_date = self.df['Date'].iloc[self.df['Price?'].last_valid_index()]
+                next_date = str(datetime.datetime.strptime(last_date, "%Y-%m-%d") + datetime.timedelta(days=1))
+                print("next_date", next_date)
 
+                # df for next date to today
+                new_df = self.get_history(next_date)
+
+                # append df to existing csv
+                new_df.to_csv(data_dir, mode = "a", header = False)
 
         except (FileNotFoundError, pd.errors.EmptyDataError, ColumnNameError):
             self.df = self.get_history("2003-1-21")
@@ -71,7 +81,8 @@ class History:
 if __name__ == "__main__":
     config = configparser.ConfigParser()
     config.read("../config.ini")
-    quandl.ApiConfig.api_key = config.get("keys", "key")
+    key = config.get("keys", "key")
+    quandl.ApiConfig.api_key = key
 
     History("NASDAQ")
-    # History("E-MINI")
+    History("E-MINI")
