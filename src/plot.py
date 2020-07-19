d0f66cf38f91d3e961b33f4cd577e4d9426bfb70
hugo-chan <hugoyhc14@gmail.com>
2020-07-19 17:42:08 +0800

    plotting and removing old data

--- src/plot.py
+++ src/plot.py
@@ -1,11 +1,14 @@
 import matplotlib.pyplot as plt
-import pandas as pd
 import matplotlib.dates as mdates
+import pandas as pd
+import datetime
 
 def plot_cumpnl(data_dir):
     df = pd.read_csv(data_dir)
     assert "Cum P&L" in list(df.columns)
     dates = pd.to_datetime(df["Date"])
+    
+    plt.figure(num=None, figsize=(10.5, 6), dpi=100)
     
     ax = plt.gca()
     # format ticks
@@ -14,9 +17,11 @@ def plot_cumpnl(data_dir):
     ax.xaxis.set_minor_locator(mdates.MonthLocator())
 
     plt.plot(dates, df['Cum P&L'], linewidth = 1.0)
-    plt.show()   
-
+    plt.title("Performance of Strategy")
+    plt.ylabel("P&L (USD)")
+    plt.xlabel("Time")
+    plt.figtext(0.13, 0.02, f"Last updated: {datetime.date.today()}", ha='left', fontsize=9)
+    plt.savefig("../data/pnl.png")
 
 if  __name__ == "__main__":
     plot_cumpnl("../data/nasdaq_e-mini.csv")
-    plt.show()
